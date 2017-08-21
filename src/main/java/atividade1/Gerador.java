b33906ebef6860ba314da439cca31dc4856c3e13
Gabriel Henrique Daniel da Silva <ghds@windows.cin.ufpe.br>
2017-08-21 12:42:06 -0300

    ajustes finais no projeto

--- src/main/java/atividade1/Gerador.java
+++ src/main/java/atividade1/Gerador.java
@@ -2,11 +2,15 @@ package atividade1;
 
 import java.io.File;
 
+/* Após implementar as suas regras léxicas no arquivo exemplo.jflex que está em src/main/resources rode essa classe "Gerador"
+   e então a classe Minijava será gerada na pasta resources.
+ */
+
 public class Gerador {
 		public static void main(String[] args) {
-			String loginAluno = "";
-			String path = "src/main/resources/";   //caminho pra pasta do arquivo
-			String arquivo = path + loginAluno;      // nome do arquivo
+			String loginAluno = "exemplo";  
+			String path = "src/main/resources/";   
+			String arquivo = path + loginAluno + "jflex";    
 			File file = new File(arquivo);	
 			jflex.Main.generate(file);	
 		}
---
+++ src/main/resources/exemplo.jflex
@@ -0,0 +1,18 @@
+%%
+
+%line
+%column
+%unicode
+//%debug
+%public
+%standalone
+%class Minijava
+%eofclose
+
+/* Insira as regras léxicas abaixo */
+
+
+    
+/* Insira as regras léxicas no espaço acima */     
+     
+. { throw new RuntimeException("Caractere ilegal! '" + yytext() + "' na linha: " + yyline + ", coluna: " + yycolumn); }
